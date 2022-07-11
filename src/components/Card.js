2e4f55b51cfaf2a7d834fab270eedf65c57833af
giorgi <giorgi.doreuli3721@gmail.com>
2022-07-12 00:00:16 +0400

    succesfully fetched data

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Card.css'
+
+function Card(props) {
+
+    
+
+  return (
+    <div className="card">
+        <div className='profile'>
+            <img src={props.image+'?v='+props.id} alt={props.name} className='img'/>
+            <h3 className='name'>{props.name}</h3>
+            <p clasaName='title'>{props.title}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import './Home.css'
+import Card from './Card.js'
 
 function Home() {
+
+    const [profiles, setProfiles] = useState([]);
+    const [showList, setShowList] = useState(false);
+
+    useEffect(() =>{
+        const getProfile = async () => {
+            const api = 'http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/24';
+            const fetchProfile = await fetch(api);
+            const fetchedProfile = await fetchProfile.json();
+            setProfiles(fetchedProfile.list);
+            setShowList(true);
+        }
+
+        getProfile();
+    })
+
+
   return (
-    <div>
-        <p>hello world</p>
+    <div className='home'>
+        <div className='profileList'>
+            {showList ?
+            profiles.map((item) =>
+                <div className='cardList'>
+                    <Card name={item.name} title={item.title} image={item.imageUrl} id={item.id}/>
+                </div>
+                    )
+            : ''}
+        </div>
     </div>
   )
 }
